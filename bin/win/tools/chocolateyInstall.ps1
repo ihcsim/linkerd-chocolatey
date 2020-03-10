af2e7d6c1f136e0bc3e7098e46da5bdcdfdd94c2
ihcsim <ihcsim@gmail.com>
2020-03-09 21:50:09 -0700

    Use variable placeholder variable for Linkerd version in install.ps1
    Signed-off-by: ihcsim <ihcsim@gmail.com>

--- bin/win/tools/chocolateyInstall.ps1
+++ bin/win/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = 'linkerd'
   fileFullPath   = "$toolsPath\linkerd.exe"
-  url            = 'https://github.com/linkerd/linkerd2/releases/download/stable-2.6.1/linkerd2-cli-stable-2.6.1-windows.exe'
+  url            = 'https://github.com/linkerd/linkerd2/releases/download/${LINKERD_VERSION}{/linkerd2-cli-${LINKERD_VERSION}-windows.exe'
   checksum       = '4ed915b10c2a76070d40bf3223f09e742777105914acaeadfc83d99ef6cf1798'
   checksumType   = 'sha256'
 }
